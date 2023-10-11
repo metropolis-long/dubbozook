1311d418a8e644d80f30ac1017c18866de4ffd46
metropolis-long <2724785961@qq.com>
2023-10-11 11:00:33 +0800

    优化结构

--- provider/src/main/java/org/bamboo/ProviderAPP.java
+++ provider/src/main/java/org/bamboo/ProviderAPP.java
@@ -3,15 +3,20 @@ package org.bamboo;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDubbo
 @MapperScan("org.bamboo.mapper")
 public class ProviderAPP {
+
+    @Value("${bamboo.name}")
+    private static  String name;
     public static void main(String[] args) {
+        System.out.println("===========================================");
+        System.out.println(name);
         SpringApplication.run(ProviderAPP.class,args);
     }
 }
