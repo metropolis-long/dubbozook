a9effaa545cd596d75301c84e564925fba673963
metropolis-long <2724785961@qq.com>
2023-10-11 11:00:51 +0800

    优化结构

--- common/src/main/java/org/bamboo/pojo/Student.java
+++ common/src/main/java/org/bamboo/pojo/Student.java
@@ -1,10 +1,14 @@
 package org.bamboo.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Student implements Serializable {
     private static final long serialVersionUID  = 1L;
     private String id;
--- common/src/main/java/org/bamboo/pojo/User.java
+++ common/src/main/java/org/bamboo/pojo/User.java
@@ -1,9 +1,17 @@
 package org.bamboo.pojo;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
-public class User {
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+
+    private static final long serialVersionUID  = 1L;
     private Long id;
     private String name;
     private String love;
