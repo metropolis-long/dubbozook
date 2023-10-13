2a744cb43a048527093a46069b0cbf0712502cee
metropolis-long <2724785961@qq.com>
2023-10-13 20:22:16 +0800

    add redis

--- provider/src/test/java/org/bamboo/SpringBootMybatisPlusTest.java
+++ provider/src/test/java/org/bamboo/SpringBootMybatisPlusTest.java
@@ -5,6 +5,10 @@ import org.bamboo.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
 
 import java.util.List;
 
@@ -13,7 +17,11 @@ public class SpringBootMybatisPlusTest {
 
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
+    @Autowired
+    private JedisPool jedisPool;
 
     @Test
     public void test(){
@@ -22,5 +30,23 @@ public class SpringBootMybatisPlusTest {
 
         System.out.println("users = " + users);
     }
+
+    @Test
+    public void testRedis(){
+        ValueOperations operations = redisTemplate.opsForValue();
+//        operations.set("name33","记得记得");
+        Object o = operations.get("name33");
+        System.out.println(o);
+        Jedis resource = jedisPool.getResource();
+        String name33 = resource.get("\\xAC\\xED\\x00\\x05t\\x00\\x06name33");
+        resource.set("name33","记得记得");
+        String name331 = resource.get("name33");
+        resource.close();
+        System.out.println(name33);
+        System.out.println(name331);
+
+
+    }
+
 
 }
